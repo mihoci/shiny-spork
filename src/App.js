29fef3ad18aa909879b2941be347e4d5e45667fb
Patrik Mihoci <pmihoci@tess.local>
2019-02-11 13:23:29 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Item from './components/item';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      item: '',
+      todoList: []
+    }
+  }
+
+  onInputChange = event => {
+    this.setState({item: event.target.value});
+  }
+
+  handleClick = event => {
+    this.addToArray();
+  }
+
+  handleKeyPress = event => {
+    console.log(this.event);
+    if(event.key === 'Enter'){
+      this.addToArray();
+    }
+  }
+
+  handleDelete = index =>{
+    let list = this.state.todoList.slice();
+    list.splice(index, 1);
+    this.setState({todoList: list});
+  }
+
+  addToArray = () => {
+    if(this.state.item===''){
+      return;
+    }
+    let list = this.state.todoList;
+    list.push(this.state.item);
+    this.setState({todoList: list});
+    this.setState({item: ''});
+  }
+
   render() {
+    const list = this.state.todoList.map((item, index) =>{
+      return <Item key={index} name={item} index={index} onDelete={this.handleDelete}/>
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>TODO List</h1>
+        <input type='text' value={this.state.item} onChange={this.onInputChange} onKeyPress={this.handleKeyPress}/>
+        <input type='button' value='ADD' onClick={this.handleClick}/>
+        {list}
       </div>
     );
   }
---
+++ src/components/item.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Item extends React.Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            crossed: false,
+        }
+    }
+
+    handleClick = () => {
+        this.setState({crossed: !this.state.crossed});
+        console.log(this.state.crossed)
+    }
+
+
+    render() {
+        const {name, onDelete, index} = this.props;
+        let cross;
+        this.state.crossed ? cross={textDecorationLine: "line-through"} : cross={};
+
+        return(
+            <div>
+                <label style={cross} onClick={this.handleClick}>{name}</label>
+                <input type='button' value='X' onClick={() => onDelete(index)}/>
+            </div>
+        );
+    }
+
+}
+
+export default Item;
